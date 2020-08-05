61371744989aaffe054f7e71759654deb8edf892
byunghui.jeun <byunghui.jeun@gooroom.kr>
2020-08-05 15:43:54 +0900

    cmd_parser

---
+++ cmd_parser.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define CMD_MAX 131070
+
+int main (int argc, char **argv) {
+  int pid;
+  char *p = NULL;
+  FILE *fp = NULL;
+  char cmdline[PATH_MAX] = { 0, };
+  char buf[CMD_MAX] = { 0, };
+
+  fprintf (stdout, "** cmd_parser **\n");
+  for (;;) {
+    pid = -1;
+    fprintf (stdout, "PID(0-32768): ");
+    fscanf (stdin, "%d", &pid);
+    if (pid < 0)
+      break;
+    snprintf (cmdline, PATH_MAX, "/proc/%d/cmdline", pid);
+    fp = fopen (cmdline, "r");
+    if (fp) {
+      fgets (buf, CMD_MAX, fp);
+      fclose (fp);
+      if (buf) {
+        fprintf (stdout, "cmd: %s", buf);
+        p = strchr (buf, '\0');
+        while (p && p[1]) {
+          fprintf (stdout, " %s", p+1);
+          p = strchr (p+1, '\0');
+        }
+        putchar ('\n');
+      } 
+    } else 
+      fprintf (stderr, "Cannot get command\n");
+    fp = NULL;
+    p = NULL;
+  }
+  fprintf (stdout, "EXIT\n");
+
+  exit (EXIT_SUCCESS);
+}
